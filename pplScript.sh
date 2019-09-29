eadfd179e75861952d2b2dd42fb8820ac5a8de20
B151359-2019 <email_not_found>
2019-09-29 22:17:50 +0100

    updated pplScript, still doesnt work, wont desplay count

--- pplScript.sh
+++ pplScript.sh
@@ -1,14 +1,22 @@
+#!/bin/sh
 #Script counting country names in example_people_data.tsv
+
 IFS=$'\t'
-declare -A countries
-while read line
-do
-country=$(cut -f7)
-countries[$country]=0
+
+declare -iA countries
+
+while read -r line; do
+  country=$(cut -f7)
+  if [[ -z "${countries[${country}]}" ]]
+  then 
+    let '++countries[$country]'
+  else
+     countries+=(["$country"]=1)
+  fi
 done < example_people_data.tsv 
 
-for cntry in ${!countries[@]}; do 
-  echo $cntry
+for cntry in "${!countries[@]}"; do 
+  echo "${cntry}" "${countries[${cntry}]}"
 done
 
-unset IFS
+unset IFS 
