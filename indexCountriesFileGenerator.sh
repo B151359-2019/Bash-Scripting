185a7dfded7eaa0174ae9f99cbc07faf3d0fac96
B151359-2019 <email_not_found>
2019-09-29 23:32:43 +0100

    indexFileGen works now, separates by old and young

--- indexCountriesFileGenerator.sh
+++ indexCountriesFileGenerator.sh
@@ -3,14 +3,19 @@
 
 count=0
 IFS=$'\t'
+rm -f *.details
 while read name email city birthday_day birthday_month birthday_year country
 do
-if test -z $name || test $country="country"
+if test -z "$name" || test "$country" == "country"
  then
- count=count
+ continue
  else
  count=$((count+1))
- echo -e "$count\t$name\t$city\t$country" >> ${country// /}.details
+ outfile="${country// /}".younger.details
+ if test "$birthday_year" -le 1980
+ then
+ outfile="${country// /}".older.details
+ fi #birthday before 1980
+ echo -e "$count\t$name\t$city\t$country" >> "$outfile"
 fi
-done < example_people_data.tsv
-
+done < example_people_data.tsv 
