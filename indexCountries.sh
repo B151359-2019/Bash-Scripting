5c3326044c78816c9bcecc88fdc1f75774915abf
B151359-2019 <email_not_found>
2019-09-29 22:26:30 +0100

    accounted for blank lines and header

--- indexCountries.sh
+++ indexCountries.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 #script indexing countries found in example_people_data
-
+unset IFS
+IFS=$'\t'
 count=0
 while read name email city birthday_day birthday_month birthday_year country; do
-count=$((count+1))
-echo -e "$count\t$country"
-done < example_people_data.tsv | head -10
+if test -z $name
+ then
+ echo -e "X\tBlank line found"
+else
+if test $country == "country"
+ then
+ echo -e "X\tHeader line found"
+ else
+  count=$((count+1))
+  echo -e "$count\t$country"
+fi #a real country
+fi #a blank line
+done < example_people_data.tsv
 
 
